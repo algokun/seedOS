e2c1bc6b326180648c91abc54315561cac9ea802
algokun <themightyking117@gmail.com>
2024-03-09 18:09:19 +0530

    removed last modified at

--- _plugins/last_modified_at_generator.rb
+++ _plugins/last_modified_at_generator.rb
@@ -1,18 +1,17 @@
-# frozen_string_literal: true
+# # frozen_string_literal: true
 
-require 'fileutils'
-require 'pathname'
-require 'jekyll-last-modified-at'
+# require 'fileutils'
+# require 'pathname'
 
-module Recents
-  # Generate change information for all markdown pages
-  class Generator < Jekyll::Generator
-    def generate(site)
-      items = site.collections['notes'].docs
-      items.each do |page|
-        timestamp = Jekyll::LastModifiedAt::Determinator.new(site.source, page.path, '%FT%T%:z').to_s
-        page.data['last_modified_at_timestamp'] = timestamp
-      end
-    end
-  end
-end
+# module Recents
+#   # Generate change information for all markdown pages
+#   class Generator < Jekyll::Generator
+#     def generate(site)
+#       items = site.collections['notes'].docs
+#       items.each do |page|
+#         timestamp = Jekyll::LastModifiedAt::Determinator.new(site.source, page.path, '%FT%T%:z').to_s
+#         page.data['last_modified_at_timestamp'] = timestamp
+#       end
+#     end
+#   end
+# end
